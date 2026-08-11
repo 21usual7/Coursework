8892db23c3fb663adeaf5fa02eed33e2d662860d
unknown <21usual7@gmail.com>
2026-08-11 11:32:39 +0300

    Добавив нові фічі для extract_features (len, is_ip, https_check, SSL check)

--- GUARD_PROJECT/app.py
+++
@@ -1,7 +0,0 @@
-
-#мне надо востановить ключ зная, какой plaitext and ciphertext. data = мой водд, он должен быть hex and len=5, 
-
-
-cipher_text = "d3418cf49e"
-cipher_text = bytes.fromhex(cipher_text)
-print(cipher_text)
--- ml/train_GUARD.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from matplotlib import pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
-
-
-data = pd.read_csv("balanced_urls.csv")
-print(data)
