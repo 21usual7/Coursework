a1866a853a43cb6f2510e333b5fb7e87a4c4b19a
unknown <21usual7@gmail.com>
2026-08-12 10:59:37 +0300

    Добавив в extract features функії extract_specific_symbols, calc_shannon_entropy, extract_entropy_features, extract_trigger_features

--- GUARD_PROJECT/database.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("example.db")
-
-db = conn.cursor()
-
-db.execute("""
-    CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, password_hash TEXT UNIQUE, created_at TEXT UNIQUE)
-""")
